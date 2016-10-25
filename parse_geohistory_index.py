49225ae62c67b68d1033c36335b3472c0702e9ed
Alexey <Alexey@Mac-Pro.local>
2016-10-25 22:15:56 +0300

    parse geohistory index

--- parse_geohistory_index.py
+++ parse_geohistory_index.py
@@ -14,7 +14,9 @@ names = []
 data = []
 vol = []
 pg = []
+pg1 = []
 names_2 = []
+
 
 with open('geohistory_index.htm', 'r', encoding='cp1251') as page:
     lines = page.readlines() ## построчный список вида ['str1', 'str2'...]
@@ -24,74 +26,49 @@ with open('geohistory_index.htm', 'r', encoding='cp1251') as page:
         line_data = re.findall(r'(<p class="name-ind">.*?</p>)', line)
         if line_data:
             if '&nbsp;' not in line_data:
-##                print(line_data) ## например: ['<p class="name-ind">Япония — <b>II:</b> 270; <b>III:</b> 190']
+##                print(line_data) ## например:
+                ## ['<p class="name-ind">Япония — <b>II:</b> 270; <b>III:</b> 190']
                 for j in line_data:
                     data.append(j) ## список, внутри строки, 423               
 
 ##    названия
 for a in data:
     name = re.findall(r'name-ind">(.*?) — <b>', a)
-    names.append(name) ## список, внутри списки, в каждом – по строке с именем или пусто, 423
+    names.append(name) ## список, внутри списки, в каждом по строке с именем или пусто, 423
     
 ##    названия второго порядка
     name_2 = re.findall(r'.*?<br>(.*?) — <b>', a)
-    names_2.append(name_2) ## список, внутри списки, в каждом – строки с именами или пусто, 423
+    names_2.append(name_2) ## список, внутри списки, в каждом строки с именами или пусто, 423
+
+##    номера томов     
+    volumes = re.findall(r'[IV]+', a)
+    vol.append(volumes) ## для каждой строки - список, такой: ['IV'],
+    ## такой: ['II', 'IV'] или такой: [], 423
+
+## номера страниц
+    vol_p = re.split(r'[IV]+', a) ##  часть строки между римскими цифрами
+    del vol_p[0]
+    pg = [re.findall(r'\d+[;,-]?\d+', element) for element in vol_p]
+    pg1.append(pg)
+##print(pg1) ## список, внутри 423 списка по числу статей, внутри один список: ['61', '366'],
+## если один том, или больше – по числу томов: [['270'], ['190']]
 
 
-    
+##    ИТОГ
+##print(names[-34])  ## ['Царское (Детское) Село (г. Пушкин)']
+##print(names_2[-34])  ## [' Китайская деревня']
+##print(vol[-34])  ## ['I', 'II', 'IV', 'IV']
+##print(pg1[-34]) ## [['76', '239'], ['485'],
+## ['13', '44', '53', '61', '62', '72', '77', '78', '81', '82', '84-88', '92', '93', '95'],
+## ['53', '83', '84', '88']]
 
+## СОЗДАЁМ CSV-ФАЙЛ С ДАННЫМИ
 
-
-####    краткие справки
-##    description = re.findall(r'</i>(.*?)<b>', a)
-##    descr.append(description) ## список, внутри списки, в каждом строка, 1226
-##    ## некоторые списки пустые, значит, справки нет (например, перенаправление)
-##            
-####  остальное
-##    vol_p = re.findall(r'<b>(.*)', a)
-##    vol_page.append(vol_p)
-####print(vol_page)  ## список, внутри списки: ['III:</b> 136, 156—158'; III: 370], 1226
-##    ## некоторые списки пустые (например, перенаправление)
-##
-####  тома
-##for i in vol_page:
-##    if i:
-##        volumes = re.findall(r'[IV]+', i[0])
-##    else:
-##        volumes = [''] ## для каждой строки - список, такой: ['IV'],
-##        ## такой: ['II', 'IV'] или такой: [''], 1226
-##    vol.append(volumes)
-##
-####  страницы
-##for i in vol_page:
-##    if i:
-##        pages = re.split(r'[IV]+', i[0])
-##    else:
-##        pages = ['']
-##    pg.append(pages) ## список, подсписки: ['', ':</b> 294; <b>', ':</b> 112'], 1226
-##pg = [[st.replace('<b>','') for st in sublist] for sublist in pg]
-##pg = [[st.replace('</b>','') for st in sublist] for sublist in pg]
-##pg = [[st.replace(';','') for st in sublist] for sublist in pg]
-##pg = [[st.replace(':','') for st in sublist] for sublist in pg]
-##pg = [[st.replace(' ','') for st in sublist] for sublist in pg]
-##for sublist in pg:
-##    if '' in sublist:
-##        sublist.remove('')
-####print(pg) ## список, подсписки: ['459—461,639', '370', '198,402,416'], 1226
-##
-####    ИТОГ
-####print(names[-891])  ## Яхонтов, Владимир Николаевич
-####print(descr[-891])  ## [' (1899—1945), актер — ']
-####print(vol[-891])  ## ['II', 'III', 'IV']
-####print(pg[-891]) ## ['459—461,639', '370', '198,402,416']
-##
-#### СОЗДАЁМ CSV-ФАЙЛ С ДАННЫМИ
-##
-##record_file = open('name_index.csv', 'w', newline='', encoding='utf8')
-##writer = csv.writer(record_file, delimiter='\t')
-##for i in range(len(names)):
-##    writer.writerow([names[i]]+[descr[i]]+[vol[i]]+[pg[i]])
-##record_file.close()
+record_file = open('geohistory_index.csv', 'w', newline='', encoding='utf8')
+writer = csv.writer(record_file, delimiter='\t')
+for i in range(len(names)):
+    writer.writerow([names[i]]+[names_2[i]]+[vol[i]]+[pg1[i]])
+record_file.close()
 
 
 
